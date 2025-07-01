e21c9d68f1509d078f4a010e17451f04dd3fde81
zothbwf <ikpliskin007@gmail.com>
2025-07-02 00:55:41 +0300

    no page update version(time polling)

--- munchkin/game/views.py
+++ munchkin/game/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-# Create your views here.
+from django.http import JsonResponse
 from .models import Game, Player
 from uuid import uuid4
 from .forms import PlayerForm
@@ -11,28 +11,17 @@ def index(request):
 
 def join_game(request, game_hash):
     context = {}
-    if request.method == "POST":
+    try:
         game = Game.objects.get(game_hash=game_hash)
-        form = PlayerForm(request.POST)
-        if form.is_valid():
-            player = Player.objects.create(
-                player_name=form.cleaned_data['player_name'], player_gender=form.cleaned_data['player_gender'], player_game=game)
-            player.save()
-        return redirect('game:join_game', game_hash=game_hash)
-    else:
-        try:
-            game = Game.objects.get(game_hash=game_hash)
-        except Game.DoesNotExist:
-            return redirect('game:index')
-        players = game.player_set.all()
-        players = players.order_by('creation_time')
-        for i in players:
-            print(i.creation_time)
-        form = PlayerForm()
-        context['players'] = players
-        context['game_hash'] = game_hash
-        context['form'] = form
-        return render(request, 'game.html', context=context)
+    except Game.DoesNotExist:
+        return redirect('game:index')
+    players = game.player_set.all()
+    players = players.order_by('creation_time')
+    form = PlayerForm()
+    context['players'] = players
+    context['game_hash'] = game_hash
+    context['form'] = form
+    return render(request, 'game.html', context=context)
 
 
 def join_game_0(request):
@@ -54,10 +43,12 @@ def delete_player(request, player_id, game_hash):
             player = Player.objects.get(id=player_id, player_game=game)
             player.delete()
         except Player.DoesNotExist:
-            pass
+            return ""
         except Game.DoesNotExist:
-            pass
-    return redirect('game:join_game', game_hash=game_hash)
+            return ""
+        context = {"player": player,
+                   "game_hash": game_hash}
+        return render(request, 'player_card.html', context=context)
 
 
 def increase_lvl(request, player_id, game_hash):
@@ -66,14 +57,16 @@ def increase_lvl(request, player_id, game_hash):
             game = Game.objects.get(game_hash=game_hash)
             player = Player.objects.get(id=player_id, player_game=game)
             update_fields = ['player_lvl']
-            player.player_lvl += 1
-            if player.player_lvl <= 10:
+            if player.player_lvl < 10:
+                player.player_lvl += 1
                 player.save(update_fields=update_fields)
         except Player.DoesNotExist:
-            pass
+            return ""
         except Game.DoesNotExist:
-            pass
-    return redirect('game:join_game', game_hash=game_hash)
+            return ""
+        context = {"player": player,
+                   "game_hash": game_hash}
+        return render(request, 'player_card.html', context=context)
 
 
 def decrease_lvl(request, player_id, game_hash):
@@ -82,14 +75,16 @@ def decrease_lvl(request, player_id, game_hash):
             game = Game.objects.get(game_hash=game_hash)
             player = Player.objects.get(id=player_id, player_game=game)
             update_fields = ['player_lvl']
-            player.player_lvl -= 1
-            if player.player_lvl >= 1:
+            if player.player_lvl > 1:
+                player.player_lvl -= 1
                 player.save(update_fields=update_fields)
         except Player.DoesNotExist:
-            pass
+            return ""
         except Game.DoesNotExist:
-            pass
-    return redirect('game:join_game', game_hash=game_hash)
+            return ""
+        context = {"player": player,
+                   "game_hash": game_hash}
+        return render(request, 'player_card.html', context=context)
 
 
 def increase_equipment(request, player_id, game_hash):
@@ -98,14 +93,15 @@ def increase_equipment(request, player_id, game_hash):
             game = Game.objects.get(game_hash=game_hash)
             player = Player.objects.get(id=player_id, player_game=game)
             update_fields = ['player_equipment']
-
             player.player_equipment += 1
             player.save(update_fields=update_fields)
         except Player.DoesNotExist:
-            pass
+            return ""
         except Game.DoesNotExist:
-            pass
-    return redirect('game:join_game', game_hash=game_hash)
+            return ""
+        context = {"player": player,
+                   "game_hash": game_hash}
+        return render(request, 'player_card.html', context=context)
 
 
 def decrease_equipment(request, player_id, game_hash):
@@ -114,14 +110,16 @@ def decrease_equipment(request, player_id, game_hash):
             game = Game.objects.get(game_hash=game_hash)
             player = Player.objects.get(id=player_id, player_game=game)
             update_fields = ['player_equipment']
-            player.player_equipment -= 1
-            if player.player_equipment >= 0:
+            if player.player_equipment > 0:
+                player.player_equipment -= 1
                 player.save(update_fields=update_fields)
         except Player.DoesNotExist:
-            pass
+            return ""
         except Game.DoesNotExist:
-            pass
-    return redirect('game:join_game', game_hash=game_hash)
+            return ""
+        context = {"player": player,
+                   "game_hash": game_hash}
+        return render(request, 'player_card.html', context=context)
 
 
 def switch_gender(request, player_id, game_hash):
@@ -137,10 +135,12 @@ def switch_gender(request, player_id, game_hash):
                 player.player_gender = 'F'
             player.save(update_fields=update_fields)
         except Player.DoesNotExist:
-            pass
+            return ""
         except Game.DoesNotExist:
-            pass
-        return redirect('game:join_game', game_hash=game_hash)
+            return ""
+        context = {"player": player,
+                   "game_hash": game_hash}
+        return render(request, 'player_card.html', context=context)
 
 
 def remove_game(request, game_hash):
@@ -154,4 +154,29 @@ def remove_game(request, game_hash):
     else:
         context = {}
         context['game_hash'] = game_hash
-        return render(request, 'confirm_removing.html', context=context)
+        return redirect('game:index')
+
+
+def load_players(request, game_hash):
+    try:
+        game = Game.objects.get(game_hash=game_hash)
+    except Game.DoesNotExist:
+        return ""
+    players = game.player_set.all()
+    players = players.order_by('creation_time')
+    form = PlayerForm()
+    context = {}
+    context['players'] = players
+    context['game_hash'] = game_hash
+    return render(request, 'players_table.html', context=context)
+
+
+def add_player(request, game_hash):
+    if request.method == "POST":
+        game = Game.objects.get(game_hash=game_hash)
+        form = PlayerForm(request.POST)
+        if form.is_valid():
+            player = Player.objects.create(
+                player_name=form.cleaned_data['player_name'], player_gender=form.cleaned_data['player_gender'], player_game=game)
+            player.save()
+        return redirect('game:load_players', game_hash=game_hash)
