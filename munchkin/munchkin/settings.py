050ac2959a8ed711c2af7e07880128a175b5bac3
zothbwf <ikpliskin007@gmail.com>
2025-07-16 01:57:46 +0300

    added https to trusted origins

--- munchkin/munchkin/settings.py
+++ munchkin/munchkin/settings.py
@@ -59,6 +59,10 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:80",
     "http://127.0.0.1:8080",
     "http://127.0.0.1:80",
+    "https://localhost:8080",
+    "https://localhost:80",
+    "https://127.0.0.1:8080",
+    "https://127.0.0.1:80",
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -66,6 +70,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:80",
     "http://127.0.0.1:8080",
     "http://127.0.0.1:80",
+    "https://localhost:8080",
+    "https://localhost:80",
+    "https://127.0.0.1:8080",
+    "https://127.0.0.1:80",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
