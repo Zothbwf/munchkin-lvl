4a17209d3ee301bb5b444b405c0ebd3364abde7e
zothbwf <ikpliskin007@gmail.com>
2025-07-18 15:28:18 +0300

    fixed ws

--- munchkin/game/routing.py
+++ munchkin/game/routing.py
@@ -1,9 +1,8 @@
-from channels.routing import URLRouter
 from django.urls import path
 from game import consumers
 
 websocket_urlpatterns = [
     path(
-        "ws/game/<game_hash>/", consumers.GameConsumer.as_asgi(), name="ws_game_update"
+        "/game/<game_hash>/", consumers.GameConsumer.as_asgi(), name="ws_game_update"
     ),
 ]
